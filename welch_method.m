d591f6ec52b2a67e9b9aa2307666cc747fb9d299
Fernando Campos <32453254+fecampos@users.noreply.github.com>
2021-11-10 15:07:18 -0500

    Update welch_method.m

--- welch_method.m
+++ welch_method.m
@@ -1,20 +1,20 @@
 function [fk Ik] = welch_method(data,window,overlap,delta,numzeros)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% spectral analysis based in welch method for spacing of the spetral estimates 
-%                                                                              
-% input: 	      data,                                                          
-% 		          window,   (number of partitions, in integer number)            
-% 		          overlap,  (overlapping data, in percentage)                    
-%       	      delta,    (spatial variation, in km, m, degrees, etc)          
-%       	      numzeros, (number of added zeros, in percentage)               
-%                                                                              
-% output:	      fk wavenumber, size: nf = round(M/2), line 27                  
-% 		          Ik modified periodigram, size nf = round(M/2), line 27         
-%                                                                              
-% author: 	    Fernando Campos (2021)                                         
-%     		      fcampos@cicese.edu.mx				                                   
-
+% spectral analysis based in welch method for spacing of the spetral estimates % 
+%                                                                              %
+% input:        data,                                                          %
+%               window,   (number of partitions, in integer number)            %
+%               overlap,  (overlapping data, in percentage)                    %
+%               delta,    (spatial variation, in km, m, degrees, etc)          %
+%               numzeros, (number of added zeros, in percentage)               %
+%                                                                              %
+% output:       fk wavenumber, size: nf = round(M/2), line 27                  %
+%               Ik modified periodigram, size nf = round(M/2), line 27         %
+%                                                                              %
+% author:       Fernando Campos (2021)                                         %
+%               fcampos@cicese.edu.mx                                          %
+%                                                                              %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 data = detrend(data);
